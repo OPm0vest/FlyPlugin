808b940dc6b1e95b44934ec29ad1d2124b6c0036
Eugene Tung <eugenetungyauwern@gmail.com>
2022-06-02 13:46:07 +0800

    6/2/2022-4

--- src/main/java/me/opm0vest/flyplugin/commands/SpeedCommand.java
+++ src/main/java/me/opm0vest/flyplugin/commands/SpeedCommand.java
@@ -28,163 +28,52 @@ public class SpeedCommand implements CommandExecutor {
             if (!(sender instanceof Player)) {
                 sender.sendMessage(Util.chat(this.main.getConfig().getString("Console_Error_Message")));
             }
-            if (args.length == 0) {
-                int speed;
-                try {
-                    speed = Integer.parseInt(args[0]);
-                } catch (NumberFormatException e) {
-                    player.sendMessage("Only integer values are allowed here");
-                    return false;
+            int speed;
+            try {
+                speed = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                player.sendMessage("Only integer values are allowed here");
+                return false;
+            }
+            if (speed < -10 || speed > 10) {
+                player.sendMessage("Speed can't be less than -10 or more than 10");
+            }
+            if (player.isFlying()) {
+                if (player.hasPermission("fly.speed")) {
+                    player.setFlySpeed((float) speed / 10.0f);
+                    player.sendMessage("Flyspeed set to " + speed);
+                } else {
+                    player.sendMessage("You don't have permission to do that");
                 }
-                if (speed < -10 || speed > 10) {
-                    player.sendMessage("Speed can't be less than -10 or more than 10");
+            } else {
+                if (player.hasPermission("walk.speed")) {
+                    player.setWalkSpeed((float) speed / 10.0f);
+                    player.sendMessage("Walk set to " + speed);
+                } else {
+                    player.sendMessage("You don't have permission to do that");
                 }
+            }
+            if (args[2] != null) {
+                Player target = Bukkit.getPlayer(args[1]);
                 if (player.isFlying()) {
                     if (player.hasPermission("fly.speed")) {
-                        player.setFlySpeed((float) speed / 10.0f);
+                        target.setFlySpeed((float) speed / 10.0f);
+                        target.sendMessage("your speed is set to " + speed);
                         player.sendMessage("Flyspeed set to " + speed);
                     } else {
                         player.sendMessage("You don't have permission to do that");
                     }
                 } else {
                     if (player.hasPermission("walk.speed")) {
-                        player.setWalkSpeed((float) speed / 10.0f);
-                        player.sendMessage("Walk set to " + speed);
+                        target.setWalkSpeed((float) speed / 10.0f);
+                        target.sendMessage("your walk speed is set to " + speed);
+                        player.sendMessage("Flyspeed set to " + speed);
                     } else {
                         player.sendMessage("You don't have permission to do that");
                     }
                 }
             }
-            if (args[2] != null) {
-                if (args.length >= 0) {
-                    Player target = Bukkit.getPlayer(args[1]);
-                    if (target != null) {
-                        int speed = 1;
-                        try {
-                            speed = Integer.parseInt(args[0]);
-                        } catch (NumberFormatException e) {
-                            player.sendMessage("Only integer values are allowed here");
-                            return true;
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 1) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (target.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.2")) {
-                            if (speed > 2) {
-                                player.sendMessage("Speed can't be more than 2");
-                            } else if (target.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                player.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                player.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 3) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 4) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 5) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 6) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 7) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 8) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 9) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        }
-                        if (player.hasPermission("fp.speed.allow.1")) {
-                            if (speed > 10) {
-                                player.sendMessage("Speed can't be more than 1");
-                            } else if (player.getAllowFlight()) {
-                                target.setFlySpeed(speed / 10.0f);
-                                target.sendMessage("Flyspeed set to " + speed);
-                            } else {
-                                target.setWalkSpeed(speed / 10.0f);
-                                target.sendMessage("Walkspeed set to " + speed);
-                            }
-                        } else {
-                            player.sendMessage(Util.chat(this.main.getConfig().getString("Insufficient_Permission_Message")));
-                        }
-                    } else {
-                        player.sendMessage("Unknown player!");
-                    }
-                }
-            }
         }
-
         return false;
     }
 }
